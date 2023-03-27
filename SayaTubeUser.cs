00210c6d6859db766a54fe64d3632952034dd845
PC1-IFLAB <>
2023-03-27 07:30:30 +0700

    menambahkan dua class SayaTubeVideo dan SayaTubeUser

---
+++ SayaTubeUser.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul6_1302213020
+{
+    class SayaTubeUser
+    {
+        private int id;
+        private List<SayaTubeVideo> uploadedVideos;
+        public string Username;
+
+        public SayaTubeUser(string username)
+        {
+            this.uploadedVideos= new List<SayaTubeVideo>();
+            Username = username;
+            this.id = new Random().Next(10000, 99999);
+        }
+
+        public int GetTotalVideoPlayCount()
+        {
+            int jumlah = 0;
+            for (int i=0;i < uploadedVideos.Count; i++)
+            {
+                jumlah += uploadedVideos[i].getPlayCount();
+            }
+
+            return jumlah;
+        }
+
+        public void AddVideo(SayaTubeVideo video)
+        {
+            this.uploadedVideos.Add(video);
+        }
+
+        public void PrintAllVideoPlayCount()
+        {
+            Console.WriteLine("User: "+ this.Username);
+            for (int i=0; i<this.uploadedVideos.Count; i++)
+            {
+                Console.WriteLine("Video "+ (i + 1) + " judul: "+ this.uploadedVideos[i].getTitle());
+            }
+        }
+    }
+}
---
+++ SayaTubeVideo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul6_1302213020
+{
+    class SayaTubeVideo
+    {
+        private int id, playCount;
+        private string title;
+
+        public SayaTubeVideo(string title)
+        {
+            this.title = title;
+            this.playCount = 0;
+            this.id = new Random().Next(10000,99999);
+        }
+
+        public void IncreasePlayCount(int playCount)
+        {
+            this.playCount += playCount;
+        }
+
+        public void PrintVideoDetails()
+        {
+            Console.WriteLine("ID: "+ this.id);
+            Console.WriteLine("Title: "+ this.title);
+            Console.WriteLine("Play Count: "+ this.playCount);
+        }
+
+        public int getPlayCount() { return this.playCount; }
+    
+        public string getTitle() { return this.title; }
+    }
+}
